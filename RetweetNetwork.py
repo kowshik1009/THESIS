05edabb16151a031f42ba315acdba318c44bcff8
kowshik1009 <36971896+kowshik1009@users.noreply.github.com>
2018-04-05 11:26:58 -0400

    Add files via upload
    This version has edges from retweeters to author of main tweet. Don't know why there is an outlier though.

--- RetweetNetwork.py
+++ RetweetNetwork.py
@@ -20,6 +20,8 @@ import pandas
 import math
 import twitter
 from twitter import *
+import networkx as nx
+from matplotlib import pyplot
 
 ckey='1Ks0gAinLy8N7ive0JR4LivSm'
 csecret='bm3MuSsIP2YL48VH9v6Ml4SdraFQwDpTqdRolpNLVBONvdwOjB'
@@ -30,12 +32,22 @@ auth=OAuthHandler(ckey,csecret)
 auth.set_access_token(atoken,asecret)
 api=tweepy.API(auth)
 
-statuses=api.user_timeline('ReallySwara', include_rts=True)
+graph=nx.DiGraph()
+username='ReallySwara'
+central_node=graph.add_node(api.get_user(username))
+
+statuses=api.user_timeline(username, include_rts=True)
 for tweet in statuses:
-    print(tweet.text)
-    print("reweeted by:")
+##    print(tweet.text)
+##    print("reweeted by:")
     who_all_retweeted=api.retweeters(tweet.id)
     for single_retweeter in who_all_retweeted:
-        print(api.get_user(single_retweeter).screen_name)
-    print("\n")
+##        print(api.get_user(single_retweeter).screen_name)
+        peripheral_node=api.get_user(single_retweeter)
+        graph.add_edge(peripheral_node,central_node)
     
+##    print("\n")
+
+nx.draw(graph)
+pyplot.show()
+nx.info(graph)
